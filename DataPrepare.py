72d86d7fe50c95dbb9212f5c280a01d7c2e5567d
qun.wei <qun.wei@advance.ai>
2019-12-17 14:39:59 +0800

    add ffm model

--- DataPrepare.py
+++ DataPrepare.py
@@ -2,6 +2,26 @@ import copy
 from sklearn.feature_extraction import DictVectorizer
 import pandas as pd
 from sklearn.preprocessing import scale
+from sklearn.preprocessing import OneHotEncoder
+
+
+features = ['Age', 'Non-Travel', 'Travel_Rarely', 'Travel_Frequently', 'DistanceFromHome', 'education_1',
+                'education_2','education_3', 'education_4', 'education_5', 'EnvironmentSatisfaction',
+                'Gender', 'JobInvolvement', 'JobLevel_1','JobLevel_2', 'JobLevel_3','JobLevel_4',"JobLevel_5",
+                'JobSatisfaction', 'MonthlyIncome', 'NumCompaniesWorked',
+                'OverTime', 'PercentSalaryHike', 'PerformanceRating', 'RelationshipSatisfaction', 'StockOptionLevel',
+                'TotalWorkingYears', 'TrainingTimesLastYear', 'WorkLifeBalance', 'YearsAtCompany', 'YearsInCurrentRole',
+                'YearsSinceLastPromotion', 'YearsWithCurrManager', 'Department_sales', 'Department_research',
+                'Department_hr',
+                'EducationField_life_sciences', 'EducationField_technical_degree', 'EducationField_marketing',
+                'EducationField_medical',
+                'EducationField_hr', 'EducationField_other', 'MaritalStatus_married', 'MaritalStatus_single',
+                'MaritalStatus_divorced',
+                'JobRole_manager', 'JobRole_research_scientist', 'JobRole_sales_executive',
+                'JobRole_sales_representative',
+                'JobRole_laboratory_technician', 'JobRole_manufacturing_director', 'JobRole_human_resources',
+                'JobRole_research_director',
+                'JobRole_healthcare_representative']
 
 
 def mapping_data(data):
@@ -10,10 +30,6 @@ def mapping_data(data):
     attrition_mapping = {"No": 0, "Yes": 1}
     train_data['Attrition'] = train_data['Attrition'].map(attrition_mapping)
 
-    # 将businessTravel mapping为 "Non-Travel": 0, "TravelRarely": 1, "TravelFrequently": 2
-    business_travel_mapping = {"Non-Travel": 0, "Travel_Rarely": 1, "Travel_Frequently": 2}
-    train_data['BusinessTravel'] = train_data['BusinessTravel'].map(business_travel_mapping)
-
     # gender mapping为 "Male": 1, "Female": 0
     gender_mapping = {"Male": 1, "Female": 0}
     train_data['Gender'] = train_data['Gender'].map(gender_mapping)
@@ -21,6 +37,91 @@ def mapping_data(data):
     # OverTime mapping为 "Yes": 1, "No": 0
     over_time_mapping = {"Yes": 1, "No": 0}
     train_data['OverTime'] = train_data['OverTime'].map(over_time_mapping)
+    return train_data
+
+
+def deal_with_business_travel(data, origin_index):
+    train_data = copy.deepcopy(data)
+    business_non_travel = [0 for i in range(train_data.shape[0])]
+    business_travel_rarely = [0 for i in range(train_data.shape[0])]
+    business_travel_frequently = [0 for i in range(train_data.shape[0])]
+    for i in range(train_data.shape[0]):
+        if train_data['BusinessTravel'].get(i + origin_index) == 'Non-Travel':
+            business_non_travel[i] = 1
+            continue
+        if train_data['BusinessTravel'].get(i + origin_index) == 'Travel_Rarely':
+            business_travel_rarely[i] = 1
+            continue
+        if train_data['BusinessTravel'].get(i + origin_index) == 'Travel_Frequently':
+            business_travel_frequently[i] = 1
+            continue
+        print(str(train_data[i:i + 1]['BusinessTravel'].get(0)) + ' error')
+    train_data['Non-Travel'] = business_non_travel
+    train_data['Travel_Rarely'] = business_travel_rarely
+    train_data['Travel_Frequently'] = business_travel_frequently
+    return train_data
+
+
+def deal_with_Education(data, origin_index):
+    train_data = copy.deepcopy(data)
+    education_1 = [0 for i in range(train_data.shape[0])]
+    education_2 = [0 for i in range(train_data.shape[0])]
+    education_3 = [0 for i in range(train_data.shape[0])]
+    education_4 = [0 for i in range(train_data.shape[0])]
+    education_5 = [0 for i in range(train_data.shape[0])]
+    for i in range(train_data.shape[0]):
+        if train_data['Education'].get(i + origin_index) == 1:
+            education_1[i] = 1
+            continue
+        if train_data['Education'].get(i + origin_index) == 2:
+            education_2[i] = 1
+            continue
+        if train_data['Education'].get(i + origin_index) == 3:
+            education_3[i] = 1
+            continue
+        if train_data['Education'].get(i + origin_index) == 4:
+            education_4[i] = 1
+            continue
+        if train_data['Education'].get(i + origin_index) == 5:
+            education_5[i] = 1
+            continue
+        print(str(train_data[i:i + 1]['Education'].get(0)) + ' error')
+    train_data['education_1'] = education_1
+    train_data['education_2'] = education_2
+    train_data['education_3'] = education_3
+    train_data['education_4'] = education_4
+    train_data['education_5'] = education_5
+    return train_data
+
+def deal_with_job_level(data, origin_index):
+    train_data = copy.deepcopy(data)
+    JobLevel_1 = [0 for i in range(train_data.shape[0])]
+    JobLevel_2 = [0 for i in range(train_data.shape[0])]
+    JobLevel_3 = [0 for i in range(train_data.shape[0])]
+    JobLevel_4 = [0 for i in range(train_data.shape[0])]
+    JobLevel_5 = [0 for i in range(train_data.shape[0])]
+    for i in range(train_data.shape[0]):
+        if train_data['JobLevel'].get(i + origin_index) == 1:
+            JobLevel_1[i] = 1
+            continue
+        if train_data['JobLevel'].get(i + origin_index) == 2:
+            JobLevel_2[i] = 1
+            continue
+        if train_data['JobLevel'].get(i + origin_index) == 3:
+            JobLevel_3[i] = 1
+            continue
+        if train_data['JobLevel'].get(i + origin_index) == 4:
+            JobLevel_4[i] = 1
+            continue
+        if train_data['JobLevel'].get(i + origin_index) == 5:
+            JobLevel_5[i] = 1
+            continue
+        print(str(train_data[i:i + 1]['JobLevel'].get(0)) + ' error')
+    train_data['JobLevel_1'] = JobLevel_1
+    train_data['JobLevel_2'] = JobLevel_2
+    train_data['JobLevel_3'] = JobLevel_3
+    train_data['JobLevel_4'] = JobLevel_4
+    train_data['JobLevel_5'] = JobLevel_5
     return train_data
 
 
@@ -157,39 +258,31 @@ def scale_train_data(train_data):
 
 
 def get_train_feature_and_train_labels(train_data,test_data,train_origin_index, test_origin_index):
-    print("train feature before scale" + str(train_data))
+    # print("train feature before scale" + str(train_data))
     train_data = scale_train_data(train_data)
-    print("train feature after scale" + str(train_data))
+    # print("train feature after scale" + str(train_data))
+    train_data = deal_with_business_travel(train_data,train_origin_index)
+    train_data = deal_with_Education(train_data,train_origin_index)
     train_data = deal_with_department(train_data,train_origin_index)
     train_data = deal_with_education_field(train_data,train_origin_index)
     train_data = deal_with_marital_status(train_data,train_origin_index)
     train_data = deal_with_job_role(train_data,train_origin_index)
+    train_data = deal_with_job_level(train_data,train_origin_index)
     train_data = mapping_data(train_data)
     dict_vectorizer = DictVectorizer(sparse=False)
-    features = ['Age', 'BusinessTravel', 'DistanceFromHome', 'Education', 'EnvironmentSatisfaction',
-                'Gender', 'JobInvolvement', 'JobLevel', 'JobSatisfaction', 'MonthlyIncome', 'NumCompaniesWorked',
-                'OverTime', 'PercentSalaryHike', 'PerformanceRating', 'RelationshipSatisfaction', 'StockOptionLevel',
-                'TotalWorkingYears', 'TrainingTimesLastYear', 'WorkLifeBalance', 'YearsAtCompany', 'YearsInCurrentRole',
-                'YearsSinceLastPromotion', 'YearsWithCurrManager', 'Department_sales', 'Department_research',
-                'Department_hr',
-                'EducationField_life_sciences', 'EducationField_technical_degree', 'EducationField_marketing',
-                'EducationField_medical',
-                'EducationField_hr', 'EducationField_other', 'MaritalStatus_married', 'MaritalStatus_single',
-                'MaritalStatus_divorced',
-                'JobRole_manager', 'JobRole_research_scientist', 'JobRole_sales_executive',
-                'JobRole_sales_representative',
-                'JobRole_laboratory_technician', 'JobRole_manufacturing_director', 'JobRole_human_resources',
-                'JobRole_research_director',
-                'JobRole_healthcare_representative']
     train_feature = train_data[features]
     train_feature = dict_vectorizer.fit_transform(train_feature.to_dict(orient='record'))
     train_labels = train_data['Attrition']
 
+    print("prepare for test data")
     test_data = scale_train_data(test_data)
+    test_data = deal_with_business_travel(test_data, test_origin_index)
+    test_data = deal_with_Education(test_data, test_origin_index)
     test_data = deal_with_department(test_data,test_origin_index)
     test_data = deal_with_education_field(test_data,test_origin_index)
     test_data = deal_with_marital_status(test_data,test_origin_index)
     test_data = deal_with_job_role(test_data,test_origin_index)
+    test_data = deal_with_job_level(test_data, test_origin_index)
     test_data = mapping_data(test_data)
     test_feature = test_data[features]
     test_feature = dict_vectorizer.fit_transform(test_feature.to_dict(orient='record'))
@@ -199,19 +292,80 @@ def get_train_feature_and_train_labels(train_data,test_data,train_origin_index, 
     return train_feature, train_labels, test_feature, test_labels
 
 
-# def prepare_test_file(test_feature):
-#     with open("test_for_fm.txt",'wb') as f:
-#         for i in range(test_feature.shape[0]):
-#             for j in range(test_feature.shape[1]):
-#                 if j != test_feature.shape[1] - 1:
-#                     if int(test_feature[i][j]) != 0:
-#                         f.write((str(j) + ':' + str(int(test_feature[i][j])) + " ").encode("utf-8"))
-#                 else:
-#                     if int(test_feature[i][j]) != 0:
-#                         f.write((str(int(test_feature[i][j])) + "\n").encode("utf-8"))
-#                     else:
-#                         f.write(("\n").encode("utf-8"))
+def prepare_train_data_for_ffm(train_data,test_data,train_origin_index, test_origin_index):
+    train_feature, train_labels, test_feature, test_labels = get_train_feature_and_train_labels(train_data,
+                                                                                                test_data,
+                                                                                                train_origin_index,
+                                                                                                test_origin_index)
 
+    field_dict = dict()
+    for feature in features:
+        if feature == 'Age':
+            field_dict[feature] = 0
+        elif feature in ['Non-Travel','Travel_Rarely','Travel_Frequently']:
+            field_dict[feature] = 1
+        elif feature in ['DistanceFromHome']:
+            field_dict[feature] = 2
+        elif feature in ['education_1', 'education_2', 'education_3', 'education_4' ,'education_5']:
+            field_dict[feature] = 3
+        elif feature in ['EnvironmentSatisfaction']:
+            field_dict[feature] = 4
+        elif feature in ['Gender']:
+            field_dict[feature] = 5
+        elif feature in ['JobInvolvement']:
+            field_dict[feature] = 6
+        elif feature in ['JobLevel_1','JobLevel_2','JobLevel_3','JobLevel_4','JobLevel_5']:
+            field_dict[feature] = 7
+        left_list = ['JobSatisfaction','MonthlyIncome','NumCompaniesWorked','OverTime','PercentSalaryHike',
+                         'PerformanceRating','RelationshipSatisfaction','StockOptionLevel','TotalWorkingYears',
+                         'TotalWorkingYears', 'TrainingTimesLastYear', 'WorkLifeBalance', 'YearsAtCompany',
+                         'YearsInCurrentRole','YearsSinceLastPromotion', 'YearsWithCurrManager']
+        if feature in left_list:
+            for i in range(len(left_list)):
+                if left_list[i] == feature:
+                    field_dict[feature] = 8 + i
+
+        if feature in ['Department_sales','Department_research','Department_hr']:
+            field_dict[feature] = 8 + len(left_list)
+        elif feature in ['EducationField_life_sciences','EducationField_technical_degree', 'EducationField_marketing',
+                         'EducationField_medical', 'EducationField_hr', 'EducationField_other']:
+            field_dict[feature] = 9 + len(left_list)
+        elif feature in ['MaritalStatus_married', 'MaritalStatus_single', 'MaritalStatus_divorced']:
+            field_dict[feature] = 10 + len(left_list)
+        elif feature in ['JobRole_manager', 'JobRole_research_scientist', 'JobRole_sales_executive','JobRole_sales_representative',
+                         'JobRole_laboratory_technician', 'JobRole_manufacturing_director', 'JobRole_human_resources',
+                         'JobRole_research_director','JobRole_healthcare_representative']:
+            field_dict[feature] = 11 + len(left_list)
+    print(train_feature.shape)
+    print(len(features))
+    print(train_feature)
+    with open('ffm_train_data.txt','w') as f:
+        for j in range(train_feature.shape[0]):
+            f.write(str(train_labels[j]))
+            f.write(' ')
+            for i in range(len(features)):
+                if train_feature[j][i] != 0:
+                    f.write(str(i))
+                    f.write(':')
+                    f.write(str(field_dict[features[i]]))
+                    f.write(":")
+                    f.write(str(train_feature[j][i]))
+                    f.write(' ')
+            f.write('\n')
+
+    with open('ffm_test_data.txt','w') as f:
+        for j in range(test_feature.shape[0]):
+            f.write(str(test_labels[j]))
+            f.write(' ')
+            for i in range(len(features)):
+                if test_feature[j][i] != 0:
+                    f.write(str(i))
+                    f.write(':')
+                    f.write(str(field_dict[features[i]]))
+                    f.write(":")
+                    f.write(str(test_feature[j][i]))
+                    f.write(' ')
+            f.write('\n')
 
 
 
--- Model.py
+++ Model.py
@@ -9,10 +9,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import xlearn
-from Predict_Leave import DataPrepare
+import DataPrepare
 import tpot
 import re
-from deepctr.models import deepfm
+
 
 
 pd.set_option('display.max_columns', None)
@@ -20,14 +20,14 @@ origin_train_data = pd.read_csv('train.csv')
 origin_test_data = pd.read_csv('test.csv')
 print(origin_test_data.shape)
 
-# print(origin_train_data.info())
-# print(origin_train_data.tail(5))
+print(origin_train_data.info())
+print(origin_train_data.head(5))
 # print(train_data.describe())
-# print("*" * 100)
+print("*" * 100)
 
 
 def lr_model(train_feature, train_labels, test_feature, test_labels):
-    # LR  结果为0.69 max_iter=1500
+    # LR  结果为0.71 max_iter=1500
     lr_model = LogisticRegression(C=1.0, penalty='l2', solver='lbfgs', max_iter=1500)
     lr_model.fit(train_feature, train_labels)
     lr_predict_result = lr_model.predict(test_feature)
@@ -37,7 +37,7 @@ def lr_model(train_feature, train_labels, test_feature, test_labels):
 
 
 def tpot_model(train_feature, train_labels, test_feature, test_labels):
-    # TPOT 结果为0.670
+    # TPOT 结果为0.743
     tpot_model = tpot.TPOTClassifier(generations=5, population_size=20, verbosity=2)
     tpot_model.fit(train_feature, train_labels)
     tpot_predict_result = tpot_model.predict(test_feature)
@@ -47,8 +47,8 @@ def tpot_model(train_feature, train_labels, test_feature, test_labels):
 
 
 def GBDT_model(train_feature, train_labels, test_feature, test_labels):
-    # GBDT  n_estimators= 120 0.71
-    gbdt_model = GradientBoostingClassifier(n_estimators=120, criterion='friedman_mse')
+    # GBDT  n_estimators= 120 0.686
+    gbdt_model = GradientBoostingClassifier(n_estimators=80, criterion='friedman_mse')
     gbdt_model.fit(train_feature, train_labels)
     gbdt_predict_result = gbdt_model.predict(test_feature)
     # gbdt_result = roc_auc_score(test_labels, gbdt_predict_result)
@@ -57,7 +57,7 @@ def GBDT_model(train_feature, train_labels, test_feature, test_labels):
 
 
 def GBDT_LR_model(train_feature, train_labels, test_feature, test_labels):
-    # 8, 6, 0.706
+    # 8, 6, 0.727
     gbdt_model = GradientBoostingClassifier(n_estimators=8, criterion='friedman_mse', max_leaf_nodes=6)
     gbdt_model.fit(train_feature, train_labels)
     print(gbdt_model.max_leaf_nodes)
@@ -84,23 +84,45 @@ def GBDT_LR_model(train_feature, train_labels, test_feature, test_labels):
 
 
 def fm_model(train_feature, train_labels, test_feature, test_labels):
-    # 0.73
-    xlearn.write_data_to_xlearn_format(train_feature, train_labels,"../Predict_leave/1.txt")
-    xlearn.write_data_to_xlearn_format(test_feature,test_labels,"../Predict_leave/2.txt")
+    # 0.734
+    xlearn.write_data_to_xlearn_format(train_feature, train_labels,"../LeavePredict/1.txt")
+    xlearn.write_data_to_xlearn_format(test_feature,test_labels,"../LeavePredict/2.txt")
     fm_model = xlearn.create_fm()
-    fm_model.setTrain("../Predict_leave/1.txt")
+    fm_model.setTrain("../LeavePredict/1.txt")
+    fm_model.disableLockFree()
     param = {'task': 'binary', 'lr': 0.2, 'lambda': 0.00002, 'metric': 'acc', 'k': 4,'opt':'sgd','epoch':50}
     fm_model.cv(param)
     fm_model.fit(param,"./model.out")
     fm_model.setSign()
-    fm_model.setTest("../Predict_leave/2.txt")
+    fm_model.setTest("../LeavePredict/2.txt")
     fm_model.predict("./model.out","./result.txt")
     with open("./result.txt",'r') as f:
         data = f.read()
     result_list = re.findall(r'[0-9]', data)
     result = [int(result_list[i]) for i in range(len(result_list))]
-    predict_result = pd.Series(result,dtype='int64')
+    # predict_result = pd.Series(result,dtype='int64')
     # print("AUC of FM is " + str(roc_auc_score(test_labels,predict_result)))
+    predict_result = np.array(result)
+    return predict_result
+
+
+def ffm_model(test_labels):
+    # 0.767
+    ffm_model = xlearn.create_ffm()
+    ffm_model.setTrain('ffm_train_data.txt')
+    param = {'task':'binary', 'lr':0.2, 'lambda':0.00002,'k':4,'epoch':400,'opt':'adagrad','metric':'acc'}
+    ffm_model.cv(param)
+    ffm_model.disableLockFree()
+    ffm_model.fit(param,'ffm_model.out')
+    ffm_model.setSign()
+    ffm_model.setTest('ffm_test_data.txt')
+    ffm_model.predict('ffm_model.out','ffm_result.txt')
+    with open("./ffm_result.txt",'r') as f:
+        data = f.read()
+    result_list = re.findall(r'[0-9]', data)
+    result = [int(result_list[i]) for i in range(len(result_list))]
+    predict_result = pd.Series(result,dtype='int64')
+    # print("AUC of FFM is " + str(roc_auc_score(test_labels,predict_result)))
     # predict_result = np.array(result)
     return predict_result
 
@@ -111,7 +133,7 @@ def sum_result(*args):
     final_result = list()
     for i in range(len(seprate_result[0])):
         temp_result = 0
-        for j in range(len(seprate_result) - 1):
+        for j in range(len(seprate_result)):
             temp_result += seprate_result[j][i]
         temp_result_list.append(temp_result)
         if seprate_result[-1][i] == 1:
@@ -131,23 +153,27 @@ def generate_submit_file(final_result):
 
 
 def for_set_well_parameter():
-    # 0.747
+    # 0.747   # 0.77  # 0.76
     train_data = origin_train_data[0:1000][:]
     test_data = origin_train_data[1001:][:]
     train_feature, train_labels, test_feature, test_labels = DataPrepare.get_train_feature_and_train_labels(train_data,
                                                                                                             test_data,
                                                                                                             0, 1001)
+    DataPrepare.prepare_train_data_for_ffm(train_data, test_data, 0, 1001)
     lr_result = lr_model(train_feature, train_labels, test_feature, test_labels)
     tpot_result = tpot_model(train_feature, train_labels, test_feature, test_labels)
     fm_result = fm_model(train_feature, train_labels, test_feature, test_labels)
+    ffm_result = ffm_model(test_labels)
     gbdt_result = GBDT_model(train_feature, train_labels, test_feature, test_labels)
     gbdt_lr_result = GBDT_LR_model(train_feature, train_labels, test_feature, test_labels)
-    # print(lr_result)
-    # print(tpot_result)
-    # print(fm_result)
-    # print(gbdt_result)
-    # print(gbdt_lr_result)
-    final_result = sum_result(lr_result, tpot_result, gbdt_result, gbdt_lr_result, fm_result)
+
+    print(lr_result)
+    print(tpot_result)
+    print(fm_result)
+    print(ffm_result)
+    print(gbdt_result)
+    print(gbdt_lr_result)
+    final_result = sum_result(tpot_result, gbdt_lr_result, fm_result, ffm_result)
     final_result = np.array(final_result)
     print(final_result)
     print("AUC of final result is " + str(roc_auc_score(test_labels, final_result)))
@@ -161,18 +187,20 @@ def predict_true_result():
     train_feature, train_labels, test_feature, test_labels = DataPrepare.get_train_feature_and_train_labels(train_data,
                                                                                                             test_data,
                                                                                                             0, 0)
-
+    DataPrepare.prepare_train_data_for_ffm(train_data, test_data, 0, 0)
     lr_result = lr_model(train_feature, train_labels, test_feature, test_labels)
     tpot_result = tpot_model(train_feature, train_labels, test_feature, test_labels)
     fm_result = fm_model(train_feature, train_labels, test_feature, test_labels)
     gbdt_result = GBDT_model(train_feature, train_labels, test_feature, test_labels)
     gbdt_lr_result = GBDT_LR_model(train_feature, train_labels, test_feature, test_labels)
+    ffm_result = ffm_model(test_labels)
     print(lr_result)
     print(tpot_result)
     print(fm_result)
     print(gbdt_result)
     print(gbdt_lr_result)
-    final_result = sum_result(lr_result, tpot_result, gbdt_result, gbdt_lr_result, fm_result)
+    print(ffm_model(test_labels))
+    final_result = sum_result(tpot_result, gbdt_lr_result, fm_result, ffm_result)
     final_result = np.array(final_result)
     print(final_result)
     print(final_result.shape)
